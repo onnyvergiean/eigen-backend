3d28daa16d23c0d5b9f17bce71c6621ec2a754e6
Onny Vergiean Saputra <onny.s@students.amikom.ac.id>
2024-01-27 15:57:09 +0700

    Add swagger documentation

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./openapi.json');
 const routers = require('./interfaces/routers');
-app.use(express.json());
 
+app.use(express.json());
+app.use(cors());
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(routers);
+
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
